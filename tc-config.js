c67e26df6531c4af6743c29c53d2cabd28317c1d
Sushiil Shettigar <dastly75@gmail.com>
2024-12-12 13:01:08 -0800

    Add files via upload

---
+++ tc-config.js
@@ -0,0 +1,22 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://2FSwBcf-AJVnMKB2cvESyIW4rWW_18mF_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+    "en-US": "Wire Fraud Awareness"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+"en-US": "&lt;p&gt;Wire Fraud is a real threat actively harming some of Redfin&amp;#39;s clients. Take this course to learn how to tell clients to spot and avoid it.&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;Complete all three lessons of this course to receive full credit.&lt;/p&gt;"
+};
+
+TC_RECORD_STORES = [];
